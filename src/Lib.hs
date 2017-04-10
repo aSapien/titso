04ed5a84a8ef0d364b8101df5e8b181c930ab6ef
Dima_Ryskin <dimar@wix.com>
2017-04-10 22:47:27 +0300

    remove build files :wheelchair:

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import qualified Data.Text.IO as T
 
 someFunc :: IO ()
-someFunc = putStrLn "Monads are awesome!"
+someFunc = T.putStrLn "Monads are awesome!"
